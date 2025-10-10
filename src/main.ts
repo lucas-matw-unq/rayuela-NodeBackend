8aea2ff8e45c4a2d45853311486229f63d76429d
Nico Alvarez <nicolas.alv3@gmail.com>
2025-10-09 22:21:04 -0300

    feat: update docker-compose for MongoDB and enable HTTPS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { config } from 'dotenv';
-
-config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
-
-console.log(`Current NODE_ENV: ${process.env.NODE_ENV}`);
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
+  // Rutas a certificados dentro de 'dist/certs' después de compilar
+  const httpsOptions = {
+    key: fs.readFileSync(path.resolve(__dirname, '../../src/certs/key.pem')),
+    cert: fs.readFileSync(path.resolve(__dirname, '../../src/certs/cert.pem')),
+  };
+
   const app = await NestFactory.create(AppModule, {
+    httpsOptions, // Habilita HTTPS
     logger: ['error', 'warn', 'log'],
   });
+
+  // Prefijo global
   app.setGlobalPrefix('api/v1');
+
+  // Configuración de Swagger
   const config = new DocumentBuilder()
     .setTitle('Rayuela backend API')
     .setDescription('API para la app de rayuela')
     .setVersion('1.0')
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
-
   SwaggerModule.setup('docs', app, document);
+
+  // CORS
   app.enableCors();
   await app.listen(3000);
 }
