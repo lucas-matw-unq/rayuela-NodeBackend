4f117b92736b4ea849e6780261c998b3250048de
Nico Alvarez <nicolas.alv3@gmail.com>
2025-10-13 22:28:52 -0300

    fix: update MongoDB image version to 4.4 and remove HTTPS options from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as fs from 'fs';
-import * as path from 'path';
 
 async function bootstrap() {
-  // Rutas a certificados dentro de 'dist/certs' después de compilar
-  const httpsOptions = {
-    key: fs.readFileSync(path.resolve(__dirname, '../../src/certs/key.pem')),
-    cert: fs.readFileSync(path.resolve(__dirname, '../../src/certs/cert.pem')),
-  };
-
   const app = await NestFactory.create(AppModule, {
-    httpsOptions, // Habilita HTTPS
     logger: ['error', 'warn', 'log'],
   });
 
